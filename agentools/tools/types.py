e37aea635a14b2874d0525418a769ce8f9e7ef82
Joong-Won Seo <joong.won.seo@gmail.com>
2024-07-26 19:20:36 +0200

    v0.5.2: restore py3.11 backwards compatibility

--- agentools/tools/types.py
+++ agentools/tools/types.py
@@ -1,1 +1,7 @@
-type JSONSchema = dict
+# type statement is new in python 3.12
+# type JSONSchema = dict
+
+# backwards-compatible version:
+from typing import TypeAlias
+
+JSONSchema: TypeAlias = dict
